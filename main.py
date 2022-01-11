b49dd2c9a6cb9e6838ababee77660662464490ff
asomala <68791501+abhisomala@users.noreply.github.com>
2022-01-11 23:38:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+weekday = input ("Is it a weekday? Please capatlize Yes or No:  ")
+vacation = input ("Is today a vacation? Please captilize Yes or No:  ")
+#Asking if weekday and if vacation and assigning to variable
+
+if weekday == 'No' or vacation == 'Yes':
+  print("Sleep in")
+else:
+  print("Wake up sleepy head")
+  
+#if is weekday and vacation outputting sleep in otherwise output wake up
